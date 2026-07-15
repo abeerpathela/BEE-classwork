ca4a8f63970bbc13493100ba7dd89fcaf873a679
abeerpathela <pathelaabeer@gmail.com>
2026-07-15 14:51:36 +0530

    LECTURE-7: In weather app made it working using open weather API key

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,23 +1,23 @@
-const form = document.getElementById("mainform");
+// const form = document.getElementById("mainform");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
 
-    const formData = new FormData(form);
+//     const formData = new FormData(form);
 
-    const data = Object.fromEntries(formData.entries());
+//     const data = Object.fromEntries(formData.entries());
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-});
+//     fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// });
 
 // console.log(fetch("https://jsonplaceholder.typicode.com/posts",
 // {
@@ -33,4 +33,49 @@ form.addEventListener("submit", (event) => {
 // }
 
 // ).then((res)=> res.json()).then((data)=> console.log(data)).catch((err)=>console.log(err)));
+
+const inputBox=document.getElementById("searchBar");
+const searchButton=document.getElementById("searchButton");
+
+const API_KEY=GENERATED_KEY_HERE_TO_BE_PLACED;
+
+
+// function buttonHandler(){
+//     let cityName=inputBox.value;
+//     let API_CALL=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=48f2e22dcbee22cd4783c78f5fc94ed2`;
+//     fetch(API_CALL).then(res=>res.json()).then((data)=>{
+//         // console.log(data.name);
+//         // console.log(data.sys.country);
+//         // console.log(data.weather[0].description);
+//         // console.log(data.main.temp);
+//         // console.log(data.main.humidity);
+//         // console.log(data.wind.speed);
+//     }).catch(err=>console.log(err));
+// }
+
+function updateUI(data) {
+    document.getElementById("cityName").innerText = data.name;
+    document.getElementById("countrycode").innerText = `, ${data.sys.country}`;
+    document.getElementById("wdesc").innerText = data.weather[0].description;
+    document.getElementById("temp").innerText = data.main.temp;
+    document.getElementById("humid").innerText = data.main.humidity;
+    document.getElementById("wspeed").innerText = data.wind.speed;
+}
+
+searchButton.addEventListener("click",async ()=>{
+    let cityName=inputBox.value;
+    try{
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`);
+        if(!response.ok){
+            throw new Error("HTTP Network Error");
+        }
+        const data = await response.json();
+        updateUI(data);
+        document.querySelector(".hidden").style.display="block";
+    }
+    catch(err){
+        console.log(err);
+    }
+});
+
 
